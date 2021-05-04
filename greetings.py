78f483d81c7c15db3910f2abadcb9c0f8888de8e
Dat Nguyen <nguyentiendat5699@gmail.com>
2021-05-04 15:28:34 -0500

    first commit

---
+++ greetings.py
@@ -0,0 +1,22 @@
+# PART 1
+user_name = input("What's your name? ")
+print(f"Hello {user_name}!")
+
+# PART 2
+user_name = input("What's your name? ")
+print(f"Hello {user_name}!")
+
+user_name = input("What's your name? ")
+print("Hey, that's my name too!")
+
+# PART 3
+name = []
+for i in range(10):
+    user_name = input("What's your name? ")
+    name.append(user_name)
+print("It was nice to meet all of you")
+print("Names are:")
+for x in range(len(name)):
+    print(name[x])
+
+# PART 4
